39b5416e0b3743df7b7f4859b4055fc5a63a9ee6
gabs1234 <gabslefloch@gmail.com>
2014-08-22 00:04:41 +0200

    added metronome object, is now functional

--- metronome.js
+++ metronome.js
@@ -1,3 +1,50 @@
 function Metronome() {
-	alert("counter");
+	console.log("Metronome turned on");
+	this.tempo = 50.0;
+	this.bpm = Math.round(60000/this.tempo);
+	this.on = false;
+	this.beatNumber = 4;
+	this.flag = 1;
+	this.subDivision = 0.25;
+
+	this.start = function() {
+		metronome.on = (metronome.on? false : true);
+		metronome.count();
+	}
+	
 }
+
+Metronome.prototype.count = function() {
+	if(metronome.on){
+		console.log("on" + " beat count: " + metronome.flag);
+		var start = setTimeout(metronome.count, metronome.bpm);
+		if(metronome.flag < metronome.beatNumber) {
+			metronome.flag++;
+		}
+		else {
+			metronome.flag = 1;
+		}
+	}
+	else if(!metronome.on) {
+		console.log("Metronome turned off");
+		clearTimeout(start);
+	}
+	else {
+		console.log("you've got a darn problem!");
+	}
+}
+
+Metronome.prototype.update = function(type) {
+	if(type === "+") {
+		metronome.tempo += 1;
+		metronome.bpm = Math.round(60000/this.tempo);
+		document.getElementById('bpm').innerHTML = metronome.tempo;
+	}
+	else if(type === "-") {
+		metronome.tempo -= 1;
+		metronome.bpm = Math.round(60000/this.tempo);
+		document.getElementById('bpm').innerHTML = metronome.tempo;
+	}
+}
+
+var metronome = new Metronome();
