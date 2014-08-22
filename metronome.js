a3416ae6154039eae0347446b1568b70d83ecc80
gabs1234 <gabslefloch@gmail.com>
2014-08-22 23:06:55 +0200

    metronome is almost usable, just need to add a click sound, features comming soon

--- metronome.js
+++ metronome.js
@@ -1,50 +1,51 @@
 function Metronome() {
 	console.log("Metronome turned on");
-	this.tempo = 50.0;
+	this.tempo = 62.0;
 	this.bpm = Math.round(60000/this.tempo);
 	this.on = false;
 	this.beatNumber = 4;
 	this.flag = 1;
 	this.subDivision = 0.25;
+	this.timer;
 
 	this.start = function() {
 		metronome.on = (metronome.on? false : true);
-		metronome.count();
-	}
-	
-}
-
-Metronome.prototype.count = function() {
-	if(metronome.on){
-		console.log("on" + " beat count: " + metronome.flag);
-		var start = setTimeout(metronome.count, metronome.bpm);
-		if(metronome.flag < metronome.beatNumber) {
-			metronome.flag++;
+		if(metronome.on) {
+			console.log("turned on");
+			metronome.count();
 		}
 		else {
+			clearTimeout(metronome.timer);
+			console.log("turned off");
 			metronome.flag = 1;
+			document.getElementById("counter").innerHTML = "0";
 		}
 	}
-	else if(!metronome.on) {
-		console.log("Metronome turned off");
-		clearTimeout(start);
-	}
-	else {
-		console.log("you've got a darn problem!");
-	}
-}
-
-Metronome.prototype.update = function(type) {
-	if(type === "+") {
+	this.add = function() {
 		metronome.tempo += 1;
 		metronome.bpm = Math.round(60000/this.tempo);
 		document.getElementById('bpm').innerHTML = metronome.tempo;
 	}
-	else if(type === "-") {
+	this.subtract = function() {
 		metronome.tempo -= 1;
 		metronome.bpm = Math.round(60000/this.tempo);
 		document.getElementById('bpm').innerHTML = metronome.tempo;
 	}
+}
+
+Metronome.prototype.count = function() {
+	console.log(metronome.flag);
+	document.getElementById("counter").innerHTML = metronome.flag;
+
+	metronome.timer = setTimeout(metronome.count, metronome.bpm);
+
+	if(metronome.flag < metronome.beatNumber) {
+		metronome.flag++;
+	}
+	else {
+		metronome.flag = 1;
+	}
+
 }
 
 var metronome = new Metronome();
